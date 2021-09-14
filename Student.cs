b80ccb2630b869098f3bf59e6065d0716695e32f
Aparna Narayan Korade <89734593+aparnakorade020@users.noreply.github.com>
2021-09-15 01:33:06 +0530

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdPartyLibrary
+{
+    public class Student
+    {
+
+        public readonly int discount;
+
+        public string FullName { get; set; }
+
+        public string Mymethod()
+        {
+            return "This is from DLL";
+        }
+
+
+
+        public Student()
+        {
+
+            discount = 12000;
+
+        }
+    }
+}
